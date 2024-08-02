aad192a0d28e5d58282fd9c99f14d2c8b736ab5c
ansariafzal <ansariafzal2112@gmail.com>
2024-08-02 12:00:38 +0530

    z index fixed

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,7 +3,7 @@ import { Button } from "./ui/button";
 
 const NavBar = () => {
   return (
-    <nav className="px-4 md:px-10 border-b flex flex-row justify-between items-center">
+    <nav className="z-30 px-4 md:px-10 border-b flex flex-row justify-between items-center">
       <a href="/">
         <img src={Logo} alt="gemini-logo" width={80} height={80} />
       </a>
